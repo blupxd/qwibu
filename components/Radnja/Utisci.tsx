61cb5995582c395151427949831f9760e3c9b85e
blupxd <matija.stefanovic050@gmail.com>
2024-06-03 00:28:02 +0200

    xd

--- components/Radnja/Utisci.tsx
+++ components/Radnja/Utisci.tsx
@@ -79,11 +79,11 @@ const Utisci: React.FC = () => {
         </div>
       </div>
 
-      {/* {reviews.slice(0, prikaz).map((review, index) => (
+      {reviews.slice(0, prikaz).map((review, index) => (
         <div key={index} className="mb-4 md:mb-12 px-4 py-6 rounded-xl bg-white shadow-lg shadow-black/20 lg:shadow-black/10">
           <div className="flex mb-2">
             <div className="mt-1 overflow-hidden relative lg:w-12 lg:h-12 md:w-16 md:h-16 w-16 h-16 rounded-full mr-2">
-              <Image src={user} alt="User" objectFit="cover" fill />
+              {/* <Image src={user} alt="User" objectFit="cover" fill /> */}
             </div>
 
             <div className="flex flex-col">
@@ -114,7 +114,7 @@ const Utisci: React.FC = () => {
             </button>
           </p>
         </div>
-      ))} */}
+      ))}
       <button
         className="text-blue-500"
         onClick={() => {
